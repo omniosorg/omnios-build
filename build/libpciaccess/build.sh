d909fe4d14a89e3f96d12f9e5268db5ade27d9ea
Thomas Merkel <tm@core.io>
2018-09-25 12:34:48 +0200

    Update license header and remove autoconf automake build dependencies

--- build/libpciaccess/build.sh
+++ build/libpciaccess/build.sh
@@ -1,27 +1,18 @@
 #!/usr/bin/bash
 #
-# {{{ CDDL HEADER START
+# {{{ CDDL HEADER
 #
-# The contents of this file are subject to the terms of the
-# Common Development and Distribution License, Version 1.0 only
-# (the "License").  You may not use this file except in compliance
-# with the License.
+# This file and its contents are supplied under the terms of the
+# Common Development and Distribution License ("CDDL"), version 1.0.
+# You may only use this file in accordance with the terms of version
+# 1.0 of the CDDL.
 #
-# You can obtain a copy of the license at usr/src/OPENSOLARIS.LICENSE
-# or http://www.opensolaris.org/os/licensing.
-# See the License for the specific language governing permissions
-# and limitations under the License.
-#
-# When distributing Covered Code, include this CDDL HEADER in each
-# file and include the License file at usr/src/OPENSOLARIS.LICENSE.
-# If applicable, add the following below this CDDL HEADER, with the
-# fields enclosed by brackets "[]" replaced with your own identifying
-# information: Portions Copyright [yyyy] [name of copyright owner]
-#
-# CDDL HEADER END }}}
+# A full copy of the text of the CDDL should have accompanied this
+# source. A copy of the CDDL is also available via the Internet at
+# http://www.illumos.org/license/CDDL.
+# }}}
 #
 # Copyright 2018 OmniOS Community Edition (OmniOSce) Association.
-# Use is subject to license terms.
 #
 # Load support functions
 . ../../lib/functions.sh
@@ -32,8 +23,6 @@ VERHUMAN=$VER
 PKG=library/libpciaccess
 SUMMARY="PCI access utility library from X.org"
 DESC="The pciaccess library wraps platform-dependent PCI access methods in a convenient library."
-
-BUILD_DEPENDS_IPS="autoconf automake"
 
 init
 download_source $PROG $PROG $VER
