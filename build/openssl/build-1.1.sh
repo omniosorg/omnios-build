aedf5915dd6042cb5ead0558f729a481fd23b108
Andy Fiddaman <illumos@fiddaman.net>
2023-03-22 17:23:09 +0000

    openssl: patch for CVE-2023-0464

--- build/openssl/build-1.1.sh
+++ build/openssl/build-1.1.sh
@@ -19,6 +19,7 @@
 
 PROG=openssl
 VER=1.1.1t
+DASHREV=1
 PKG=library/security/openssl-11
 SUMMARY="Cryptography and SSL/TLS Toolkit"
 DESC="A toolkit for Secure Sockets Layer and Transport Layer protocols "
