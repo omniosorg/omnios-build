f9386d5732a97830660ab438b153703d6d378a2c
Andy Fiddaman <illumos@fiddaman.net>
2023-09-01 09:54:04 +0000

    setuptools - update from 68.0.0 to 68.1.2

--- build/python311/setuptools/build.sh
+++ build/python311/setuptools/build.sh
@@ -18,7 +18,7 @@
 
 PKG=library/python-3/setuptools-311
 PROG=setuptools
-VER=68.0.0
+VER=68.1.2
 SUMMARY="Python package management"
 DESC="Easily download, build, install, upgrade, and uninstall Python packages"
 
