7d0415f32d848ad88e020e3c5ba468d13b52c254
Andy Fiddaman <illumos@fiddaman.net>
2023-11-10 13:23:14 +0000

    meson - update from 1.2.1 to 1.2.3

--- build/python311/meson/build.sh
+++ build/python311/meson/build.sh
@@ -18,7 +18,7 @@
 
 PKG=library/python-3/meson-311
 PROG=meson
-VER=1.2.1
+VER=1.2.3
 SUMMARY="The Meson Build system"
 DESC="An open source build system meant to be both extremely fast, "
 DESC+="and, even more importantly, as user friendly as possible"
