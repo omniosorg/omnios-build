a01ae5d95adc17f8b44d1af9e30855ce329fa969
Andy Fiddaman <illumos@fiddaman.net>
2024-01-12 14:37:30 +0000

    attrs - update from 23.1.0 to 23.2.0

--- build/python311/attrs/build.sh
+++ build/python311/attrs/build.sh
@@ -12,13 +12,13 @@
 # http://www.illumos.org/license/CDDL.
 # }}}
 #
-# Copyright 2023 OmniOS Community Edition (OmniOSce) Association.
+# Copyright 2024 OmniOS Community Edition (OmniOSce) Association.
 #
 . ../../../lib/build.sh
 
 PKG=library/python-3/attrs-311
 PROG=attrs
-VER=23.1.0
+VER=23.2.0
 SUMMARY="Python attrs"
 DESC="Classes Without Boilerplate"
 
