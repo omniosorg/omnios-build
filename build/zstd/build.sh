be7b113f65270d3ed2b88be2ed0c87219803aafa
Marco van Wieringen <marco.van.wieringen@planets.elm.net>
2024-03-30 13:00:23 +0100

    zstd: update to 1.5.6

--- build/zstd/build.sh
+++ build/zstd/build.sh
@@ -12,12 +12,12 @@
 # http://www.illumos.org/license/CDDL.
 # }}}
 
-# Copyright 2022 OmniOS Community Edition (OmniOSce) Association.
+# Copyright 2024 OmniOS Community Edition (OmniOSce) Association.
 
 . ../../lib/build.sh
 
 PROG=zstd
-VER=1.5.5
+VER=1.5.6
 PKG=compress/zstd
 SUMMARY="Zstandard"
 DESC="Zstandard is a real-time compression algorithm, providing high "
