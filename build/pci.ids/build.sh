d7a2b7903c24bfe83cbdb9608568981ef1ecca35
Andy Fiddaman <illumos@fiddaman.net>
2024-07-20 12:32:46 +0000

    Ship /usr/share/pci.ids as symlink

--- build/pci.ids/build.sh
+++ build/pci.ids/build.sh
@@ -21,25 +21,11 @@
 # CDDL HEADER END }}}
 #
 # Copyright 2015 OmniTI Computer Consulting, Inc.  All rights reserved.
-# Copyright 2022 OmniOS Community Edition (OmniOSce) Association.
-# Use is subject to license terms.
+# Copyright 2024 OmniOS Community Edition (OmniOSce) Association.
 #
 . ../../lib/build.sh
 
 PROG=pci.ids
-FORMAT=2.2
-
-PCIIDS=usr/src/data/hwdata/$PROG
-check_for_prebuilt $PCIIDS
-logcmd cp $PREBUILT_ILLUMOS/$PCIIDS $SRCDIR/$PROG
-
-SNAPDATE=`$NAWK '
-    $2 == "Version:" {
-        gsub(/\./, "")
-        print $3
-    }' $SRCDIR/$PROG`
-VER=${FORMAT}.${SNAPDATE}
-VERHUMAN="v$FORMAT snapshot from $SNAPDATE"
 PKG=system/pciutils/pci.ids
 SUMMARY="Repository of all known IDs used in PCI devices"
 DESC="Repository of all known IDs used in PCI devices: IDs of vendors, "
@@ -47,31 +33,43 @@ DESC+="devices, subsystems and device classes. It is used in various programs "
 DESC+="(like pciutils) to display full human-readable names instead of "
 DESC+="cryptic numeric codes."
 
-set_arch 64
+# We require a built illumos tree alongside this. This is where our compat
+# symlink will point, so we determine the package version from there.
+FORMAT=2.2
+PCIIDS=usr/src/data/hwdata/$PROG
+check_for_prebuilt $PCIIDS
+SNAPDATE=`$NAWK '
+    $2 == "Version:" {
+        gsub(/\./, "")
+        print $3
+    }' $PREBUILT_ILLUMOS/$PCIIDS`
+VER=${FORMAT}.${SNAPDATE}
+VERHUMAN="v$FORMAT snapshot from $SNAPDATE"
 
-# Nothing to configure or build, just package
-make_install() {
-    logmsg "--- make install"
-    logcmd $MKDIR -p $DESTDIR$PREFIX/share || \
-        logerr "------ Failed to create destination directory."
-    logcmd $CP -p ${PROG}.gz $DESTDIR$PREFIX/share/ || \
-        logerr "------ Failed to copy file into place."
-}
+create_manifest()
+{
+    local mf=$1
+    cat << EOM > $mf
+set name=pkg.fmri \
+ value=pkg://\$(PKGPUBLISHER)/system/pciutils/$PROG@$VER,\$(SUNOSVER)-\$(PVER)
+set name=pkg.summary value="$SUMMARY"
+set name=pkg.description value="$DESC"
+set name=pkg.human-version value="$VER"
+set name=publisher value=\$(PKGPUBEMAIL)
 
-build() {
-    pushd $TMPDIR > /dev/null
-    logcmd $GZIP -c $SRCDIR/$PROG > ${PROG}.gz
-    make_install
-    popd > /dev/null
+link path=usr/share/pci.ids target=hwdata/pci.ids
+
+license ../../LICENSE license=CDDL
+EOM
 }
 
 init
 prep_build
-build
-((EXTRACT_MODE)) && exit
-make_package
+
+manifest=$TMPDIR/$PKGE.p5m
+create_manifest $manifest
+publish_manifest $PKG $manifest
 clean_up
-logcmd rm -f $SRCDIR/$PROG
 
 # Vim hints
 # vim:ts=4:sw=4:et:fdm=marker
