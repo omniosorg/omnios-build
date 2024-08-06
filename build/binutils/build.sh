0fac26ca72b0b91594426c8a9f5ff649c8e32007
Andy Fiddaman <illumos@fiddaman.net>
2024-08-06 23:29:02 +0000

    binutils - update from 2.42 to 2.43

--- build/binutils/build.sh
+++ build/binutils/build.sh
@@ -19,7 +19,7 @@
 . ../../lib/build.sh
 
 PROG=binutils
-VER=2.42
+VER=2.43
 PKG=developer/gnu-binutils
 SUMMARY="GNU binary utilities"
 DESC="A set of programming tools for creating and managing binary programs, "
