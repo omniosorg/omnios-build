311d2cf6632d2c2da803f2e2bef4fc550f266aa6
Andy Fiddaman <illumos@fiddaman.net>
2024-09-30 16:25:33 +0000

    openssh - update from 9.8p1 to 9.9p1

--- build/openssh/build.sh
+++ build/openssh/build.sh
@@ -18,7 +18,7 @@
 . ../../lib/build.sh
 
 PROG=openssh
-VER=9.8p1
+VER=9.9p1
 PKG=network/openssh
 SUMMARY="OpenSSH Client and utilities"
 DESC="OpenSSH Secure Shell protocol Client and associated Utilities"
