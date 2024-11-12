12ee87e30877bb17ead5ea0ffddb52eb67a4e1af
Andy Fiddaman <illumos@fiddaman.net>
2024-11-12 12:44:46 +0000

    bind - update from 9.18.30 to 9.18.31

--- build/bind/build.sh
+++ build/bind/build.sh
@@ -18,12 +18,12 @@
 . ../../lib/build.sh
 
 PROG=bind
-VER=9.18.30
+VER=9.18.31
 PKG=network/dns/bind
 SUMMARY="BIND DNS tools"
 DESC="Client utilities for DNS lookups"
 
-LIBUVVER=1.49.1
+LIBUVVER=1.49.2
 XFORM_ARGS+=" -DLIBUV=$LIBUVVER"
 
 # This package ships private shared libraries in $PREFIX/lib/dns that are only
