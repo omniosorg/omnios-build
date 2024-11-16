411af6b22f7324ed90a8ab6b5617dc470b8bf33c
Dominik Hassler <hadfl@omnios.org>
2024-11-16 13:03:47 +0000

    openjdk11: update to 11.0.25+9

--- build/openjdk11/build.sh
+++ build/openjdk11/build.sh
@@ -17,7 +17,7 @@
 . ../../lib/build.sh
 
 PROG=openjdk
-VER=11.0.24+8
+VER=11.0.25+9
 PKG=runtime/java/openjdk11
 SUMMARY="openjdk ${VER%%.*}"
 DESC="Open-source implementation of the eleventh edition of the "
