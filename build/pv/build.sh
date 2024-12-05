240408da024eef649fdbe16be8ff2ae1ee3cee9c
Andy Fiddaman <illumos@fiddaman.net>
2024-12-05 10:35:23 +0000

    pv - update from 1.9.0 to 1.9.7

--- build/pv/build.sh
+++ build/pv/build.sh
@@ -27,7 +27,7 @@
 . ../../lib/build.sh
 
 PROG=pv
-VER=1.9.0
+VER=1.9.7
 PKG=shell/pipe-viewer
 SUMMARY="Pipe Viewer"
 DESC="pv - a terminal-based tool for monitoring the progress of data through a pipeline."
