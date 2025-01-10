070e35d27d4b76c7f38ac95ccdaa6044458fde1e
Andy Fiddaman <illumos@fiddaman.net>
2025-01-10 14:20:02 +0000

    gettext - update from 0.22.5 to 0.23.1

--- build/gettext/build.sh
+++ build/gettext/build.sh
@@ -13,12 +13,12 @@
 # }}}
 #
 # Copyright 2016 OmniTI Computer Consulting, Inc.  All rights reserved.
-# Copyright 2024 OmniOS Community Edition (OmniOSce) Association.
+# Copyright 2025 OmniOS Community Edition (OmniOSce) Association.
 
 . ../../lib/build.sh
 
 PROG=gettext
-VER=0.22.5
+VER=0.23.1
 PKG=text/gnu-gettext
 SUMMARY="gettext - GNU gettext utility"
 DESC="GNU gettext - GNU gettext utility"
