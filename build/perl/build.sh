7e306e85c5db98aef2a34b24f42204462af1a85b
Dominik Hassler <hadfl@omnios.org>
2025-01-21 16:12:04 +0000

    perl: update to 5.40.1

--- build/perl/build.sh
+++ build/perl/build.sh
@@ -13,13 +13,13 @@
 # }}}
 
 # Copyright 2011-2017 OmniTI Computer Consulting, Inc.  All rights reserved.
-# Copyright 2024 OmniOS Community Edition (OmniOSce) Association.
+# Copyright 2025 OmniOS Community Edition (OmniOSce) Association.
 
 . ../../lib/build.sh
 
 PROG=perl
 PKG=runtime/perl
-VER=5.40.0
+VER=5.40.1
 MAJVER=${VER%.*}
 SUMMARY="Perl $MAJVER Programming Language"
 DESC="A highly capable, feature-rich programming language"
