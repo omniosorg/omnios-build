308ed60bf7cb6ee794a35a33adb05c420380679d
Andy Fiddaman <illumos@fiddaman.net>
2025-05-22 22:34:41 +0000

    nss - update from 3.110 to 3.111

--- build/nss/build.sh
+++ build/nss/build.sh
@@ -17,7 +17,7 @@
 . ../../lib/build.sh
 
 PROG=nss
-VER=3.110
+VER=3.111
 PKG=system/library/mozilla-nss
 SUMMARY="Network Security Services"
 DESC="Network Security Services (NSS) is a set of libraries designed to "
