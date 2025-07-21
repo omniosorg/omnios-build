ad9cdaadac8794b01cbf696d45b0bbad742665b8
Andy Fiddaman <illumos@fiddaman.net>
2025-07-21 14:27:25 +0000

    sqlite3 - update from 3500100 to 3500300

--- build/sqlite3/build.sh
+++ build/sqlite3/build.sh
@@ -18,7 +18,7 @@
 . ../../lib/build.sh
 
 PROG=sqlite-autoconf
-VER=3500100
+VER=3500300
 PKG=database/sqlite-3
 SUMMARY="SQL database engine library"
 DESC="SQLite is a self-contained, high-reliability, embedded, full-featured, "
