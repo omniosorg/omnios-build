f266af11dfc8f71f10690f424ba36ff9e60b15a4
Andy Fiddaman <illumos@fiddaman.net>
2025-08-27 14:04:45 +0000

    openssl - update from 3.5.1 to 3.5.2

--- build/openssl/build-3.sh
+++ build/openssl/build-3.sh
@@ -19,7 +19,7 @@
 . common.sh
 
 PROG=openssl
-VER=3.5.1
+VER=3.5.2
 PKG=library/security/openssl-3
 SUMMARY="Cryptography and SSL/TLS Toolkit"
 DESC="A toolkit for Secure Sockets Layer and Transport Layer protocols "
