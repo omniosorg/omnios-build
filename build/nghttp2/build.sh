952b43163cbc3f395583003d817a9d4dc4c054d0
Andy Fiddaman <illumos@fiddaman.net>
2025-09-22 15:58:11 +0000

    nghttp2 - update from 1.66.0 to 1.67.1

--- build/nghttp2/build.sh
+++ build/nghttp2/build.sh
@@ -18,7 +18,7 @@
 . ../../lib/build.sh
 
 PROG=nghttp2
-VER=1.66.0
+VER=1.67.1
 PKG=library/nghttp2
 SUMMARY="nghttp2: HTTP/2 C Library"
 DESC="An implementation of the Hypertext Transfer Protocol version 2 in C"
