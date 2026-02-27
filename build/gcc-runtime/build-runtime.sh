bcaeee2ec2c4fda720dd994d608692d3cf1ba554
Andy Fiddaman <illumos@fiddaman.net>
2026-02-27 22:54:22 +0000

    Add gcc15

--- build/gcc-runtime/build-runtime.sh
+++ build/gcc-runtime/build-runtime.sh
@@ -12,14 +12,14 @@
 # http://www.illumos.org/license/CDDL.
 # }}}
 #
-# Copyright 2024 OmniOS Community Edition (OmniOSce) Association.
+# Copyright 2026 OmniOS Community Edition (OmniOSce) Association.
 
 . ../../lib/build.sh
 . common.sh
 
 PKG=system/library/gcc-runtime
 PROG=libgcc_s
-VER=14
+VER=15
 SUMMARY="GNU compiler runtime dependencies"
 DESC="$SUMMARY"
 
