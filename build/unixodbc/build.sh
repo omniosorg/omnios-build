222ab36d79cae5736a87980466151fdebabcddf5
Andy Fiddaman <illumos@fiddaman.net>
2026-03-05 11:41:45 +0000

    unixodbc: fix build with gcc15

--- build/unixodbc/build.sh
+++ build/unixodbc/build.sh
@@ -23,11 +23,9 @@ PKG=library/unixodbc
 SUMMARY="The UnixODBC Subsystem and SDK"
 DESC="UnixODBC - The UnixODBC Subsystem and SDK"
 
-# This does not yet build with gcc 15
-set_gccver 14
-
 # Requires the new definition of getpwuid_r()
 set_standard XPG6
+set_cstandard gnu17
 
 CONFIGURE_OPTS="
     --includedir=$PREFIX/include/odbc
