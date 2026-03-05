7aef8d286eddf2d009ca1f18b8d23477f5e35e76
Andy Fiddaman <illumos@fiddaman.net>
2026-03-05 10:54:11 +0000

    readline: fix build with gcc15

--- build/readline/build.sh
+++ build/readline/build.sh
@@ -13,7 +13,7 @@
 # }}}
 #
 # Copyright 2016 OmniTI Computer Consulting, Inc.  All rights reserved.
-# Copyright 2025 OmniOS Community Edition (OmniOSce) Association.
+# Copyright 2026 OmniOS Community Edition (OmniOSce) Association.
 #
 . ../../lib/build.sh
 
@@ -22,9 +22,6 @@ VER=8.3
 PKG=library/readline
 SUMMARY="GNU readline"
 DESC="GNU readline library"
-
-# This does not yet build with gcc 15
-set_gccver 14
 
 # Previous versions that also need to be built and packaged since compiled
 # software may depend on it.
@@ -44,7 +41,8 @@ prep_build
 # Skip previous versions for cross compilation
 pre_build() { ! cross_arch $1; }
 
-save_variables BUILDDIR EXTRACTED_SRC
+save_buildenv BUILDDIR EXTRACTED_SRC
+set_cstandard gnu17
 for pver in $PVERS; do
     [ -z "$FLAVOR" -o "$FLAVOR" = $pver ] || continue
     note -n "Building previous version: $pver"
@@ -53,7 +51,7 @@ for pver in $PVERS; do
     patch_source patches-${pver%%.*}
     ((EXTRACT_MODE == 0)) && build
 done
-restore_variables BUILDDIR EXTRACTED_SRC
+restore_buildenv BUILDDIR EXTRACTED_SRC
 unset -f pre_build
 
 [ -z "$FLAVOR" -o "$FLAVOR" = $VER ] || exit 0
