c1dc168860d431f381c7343d6be27d3e3ee8aec4
Andy Fiddaman <illumos@fiddaman.net>
2026-03-18 11:05:04 +0000

    cffi - update from 2.0.0b1 to 2.0.0

--- build/python313/cffi/build.sh
+++ build/python313/cffi/build.sh
@@ -12,13 +12,13 @@
 # http://www.illumos.org/license/CDDL.
 # }}}
 #
-# Copyright 2025 OmniOS Community Edition (OmniOSce) Association.
+# Copyright 2026 OmniOS Community Edition (OmniOSce) Association.
 
 . ../../../lib/build.sh
 
 PKG=library/python-3/cffi-313
 PROG=cffi
-VER=2.0.0b1
+VER=2.0.0
 SUMMARY="cffi"
 DESC="Foreign Function Interface for Python calling C code"
 
