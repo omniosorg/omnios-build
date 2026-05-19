efc8e0d3b87f06fadc11b9f70d626082e240c673
Andy Fiddaman <illumos@fiddaman.net>
2026-05-19 12:12:37 +0000

    pycurl - update from 7.45.7 to 7.46.0

--- build/python313/pycurl/build.sh
+++ build/python313/pycurl/build.sh
@@ -18,7 +18,7 @@
 
 PKG=library/python-3/pycurl-313
 PROG=pycurl
-VER=7.45.7
+VER=7.46.0
 SUMMARY="Python bindings for libcurl"
 DESC="PycURL provides a thin layer of Python bindings on top of libcurl."
 
