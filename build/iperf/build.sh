aa81a230a57c847bf3602b808b13e08b66f2350f
Andy Fiddaman <illumos@fiddaman.net>
2026-06-30 17:25:27 +0000

    iperf - update from 3.20 to 3.21

--- build/iperf/build.sh
+++ build/iperf/build.sh
@@ -21,12 +21,12 @@
 # CDDL HEADER END }}}
 #
 # Copyright 2016 OmniTI Computer Consulting, Inc.  All rights reserved.
-# Copyright 2025 OmniOS Community Edition (OmniOSce) Association.
+# Copyright 2026 OmniOS Community Edition (OmniOSce) Association.
 #
 . ../../lib/build.sh
 
 PROG=iperf
-VER=3.20
+VER=3.21
 PKG=network/test/iperf
 SUMMARY="iperf network testing tool"
 DESC="A tool for active measurements of the maximum achievable bandwidth on "
